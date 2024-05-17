9e064f4add04c4676932f154beb6f7d58b079089
renaudbaussart <renaud.baussart@proton.me>
2024-05-17 16:39:43 +0200

    Finish the josephus problem!!!!!!!!

---
+++ src/josephus-problem.js
@@ -0,0 +1,32 @@
+function prisonerCircleMaker(nbr) {
+    let outputArray = [];
+    for (let i = 1; i <= nbr; i++)
+        outputArray.push(i);
+    }
+    return outputArray;
+}
+
+function killSelector(nbrLeftPrisoner, nthPrisonnerKill) {
+    if (nbrLeftPrisoner < nthPrisonnerKill) {
+        nthPrisonnerKill -= nbrLeftPrisoner;
+        return killSelector(nbrLeftPrisoner, nthPrisonnerKill);
+    } else {
+        return nthPrisonnerKill;
+    }
+}
+
+function josephus(circlePrisonerSize, killOrder) {
+    let prisonnerLeftArray = prisonerCircleMaker(circlePrisonerSize);
+    let currentIndex = 0;
+
+    for (let i = prisonnerLeftArray.length; i > 1; i--) {
+        currentIndex = (currentIndex + killOrder - 1) % prisonnerLeftArray.length;
+        prisonnerLeftArray.splice(currentIndex, 1);
+    }
+
+    return prisonnerLeftArray[0];
+}
+console.log(josephus(5,3));
+console.log(josephus(6,2));
+console.log(josephus(7,2));
+console.log(josephus(8,2));
