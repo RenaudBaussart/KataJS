6032371013e5cf2407f9a25e5ed434bf46f5651c
renaudbaussart <renaud.baussart@proton.me>
2024-05-17 11:23:20 +0200

    fix(3-chestboard.js): take in acount space char in the loop

--- src/3-chestboard.js
+++ src/3-chestboard.js
@@ -1,8 +1,8 @@
 let chestOutputString ='';
-let bindingSizeBoard = 16;
+let bindingSizeBoard = 8;
 for(let i = 1; i <= bindingSizeBoard; i++){
 
-    for(let j = 1; j <= bindingSizeBoard; j++){
+    for(let j = 1; j <= bindingSizeBoard / 2; j++){
 
         if(i % 2 != 0){
             chestOutputString += ' #';
