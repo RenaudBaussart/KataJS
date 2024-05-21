6593de465e35e58fd4caeaa112140b7cfc208987
renaudbaussart <renaud.baussart@proton.me>
2024-05-21 15:05:39 +0200

    done kata

---
+++ src/8kyu/Beginer-series-2.js
@@ -0,0 +1,8 @@
+/*
+Clock shows h hours, m minutes and s seconds after midnight.
+
+Your task is to write a function which returns the time since midnight in milliseconds.
+*/
+function past(h, m, s){
+    return(h * 60 * 60 * 1000) + (m * 60 * 1000) + (s * 1000);
+  }
